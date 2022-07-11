b1a42e5c9b1195ae05e0697a9833b9ce391a2a6c
David Pineda <dspinedao@gmail.com>
2022-07-11 12:31:25 -0500

    cambio en h1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App= ()=>{
   return(
     <div className="active">
-      <h1>Hola Mundo</h1>
+      <h1>Mi primera aplicacion</h1>
       <p>Este es mi primera aplicacion en React, esta padrisimo.</p>
     </div>
   )
