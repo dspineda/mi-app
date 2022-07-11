b160b788f42acb74a89d66198e5ee744270a66ac
David Pineda <dspinedao@gmail.com>
2022-07-11 12:21:39 -0500

    feacture: add my first use with react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App= ()=>{
+  return(
+    <div className="active">
+      <h1>Hola Mundo</h1>
+      <p>Este es mi primera aplicacion en React, esta padrisimo.</p>
+    </div>
+  )
+
+}
+
+export default App;
